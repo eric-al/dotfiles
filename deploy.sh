00efdb133f88cdd019dc93b141d91072491bbd3d
Eric Reeves <eric@alertlogic.com>
2013-04-05 10:34:30 -0500

    Added knife-aliases plugin

--- deploy.sh
+++ deploy.sh
@@ -2,3 +2,6 @@
 ln -sf ~/dotfiles/.zshrc ~/.zshrc
 ln -sf ~/dotfiles/themes/litex.zsh-theme ~/.oh-my-zsh/themes/litex.zsh-theme
 ln -sf ~/dotfiles/.tmux.conf ~/.tmux.conf
+
+mkdir -p ~/.oh-my-zsh/plugins/knife-aliases/
+ln -sf ~/dotfiles/plugins/knife-aliases/knife-aliases.plugin.zsh  ~/.oh-my-zsh/plugins/knife-aliases/knife-aliases.plugin.zsh
